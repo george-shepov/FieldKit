b395ca4a07da18f1f892a189892ff5466a241244
Giorgiy Shepov <40531545+george-shepov@users.noreply.github.com>
2026-07-02 04:05:19 -0400

    Cache Vocabulary Expander and launcher extension

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "fieldkit-shell-v15";
+const CACHE_NAME = "fieldkit-shell-v16";
 const OFFLINE_FALLBACK = "/index.html";
 
 const PRECACHE_URLS = [
@@ -12,6 +12,7 @@ const PRECACHE_URLS = [
   "/shared/privacy-mode.js",
   "/shared/global-auth.js",
   "/shared/pwa-init.js",
+  "/shared/app-extensions.js",
   "/shared/ui-tweaks-runtime.js",
   "/shared/icons/tictak-icon-192.png",
   "/shared/icons/tictak-icon-512.png",
@@ -56,17 +57,26 @@ const PRECACHE_URLS = [
   "/tic-tac-toe/index.html",
   "/time-tracker/index.html",
   "/ui-tweaker/index.html",
+  "/vocabulary-expander/index.html",
   "/wishlist/index.html",
   "/drivers-license/DriversLicensePrep.html",
   "/games/reversi.html",
   "/games/game-academy.html"
 ];
 
+const OPTIONAL_SHARED_URLS = [
+  "/Vocabulary-Expander/vocabulary-toolkit.css",
+  "/Vocabulary-Expander/vocabulary-toolkit.js"
+];
+
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches
       .open(CACHE_NAME)
-      .then((cache) => cache.addAll(PRECACHE_URLS))
+      .then(async (cache) => {
+        await cache.addAll(PRECACHE_URLS);
+        await Promise.allSettled(OPTIONAL_SHARED_URLS.map((url) => cache.add(url)));
+      })
       .then(() => self.skipWaiting())
   );
 });
